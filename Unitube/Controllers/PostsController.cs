c3b7a4196b4077d76a4d4271acc59ed6bc0c94c0
conne <conne@DESKTOP-RAMBSM2>
2019-02-12 11:02:05 -0500

    Front-end changes

---
+++ Unitube/Controllers/PostsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Unitube.Models;
+
+namespace Unitube.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: Posts/Random
+        public ActionResult Random()
+        {
+            var post = new Post() { Name = "Test!" };
+
+            return View(post);
+        }
+    }
+}
--- Unitube/Global.asax
+++ Unitube/Global.asax
@@ -1,1 +1,2 @@
 ﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Unitube.MvcApplication" Language="C#" %>
+
