e9a2585389ff102d08389993fcfe9c571f0dd7a7
conne <conne@DESKTOP-RAMBSM2>
2019-02-13 19:14:58 -0500

    Database & Frontend changes

---
+++ Unitube/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Unitube.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+
+        }
+
+
+        //Registration POST action
+
+
+
+        //Verify Email
+
+
+
+        //Login
+
+
+
+        //Logout
+
+
+
+    }
+}
